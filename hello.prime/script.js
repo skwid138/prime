387e162ba3826912b4a6b5175e33232214b2c5a1
skwid138 <skwid138@gmail.com>
2017-07-18 09:55:53 -0500

    completed youtube video #4

--- hello.prime/script.js
+++ hello.prime/script.js
@@ -1,1 +1,38 @@
+/*
 console.log( 'Hello World from Prime!' );
+
+
+var testVar = 'some value';
+
+console.log(testVar);
+
+// typeof [varName] will return if it's a num, string, or boolean
+
+*/
+
+var clicks = 0;
+var tau = 6.28318;
+
+// not sure why we make the function a variable rather than declare the function
+//function addClick (){};
+
+var addClick = function( ) {
+  console.log( 'in addClick');
+  clicks++; //increments "clicks variable"
+  console.log( 'clicks:', clicks );
+  // check if greater than tau
+  console.log( isGreaterThan( clicks, tau ));
+
+}; // end addClick
+
+var isGreaterThan = function ( number0, number1 ) {
+  if ( number0 > number1) {
+    return true;
+  } else {
+    return false;
+    // I dont think this else is needed, but it's in the video
+  }
+};
+
+// this function can now be used over and over
+isGreaterThan( 120, 1);
