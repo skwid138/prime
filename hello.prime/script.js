d41fe212198021831ccd259f099aedb7a4794d51
skwid138 <skwid138@gmail.com>
2017-07-18 14:33:16 -0500

    video 8/9 prototypes added notes and code from video

--- hello.prime/script.js
+++ hello.prime/script.js
@@ -37,3 +37,34 @@ var isGreaterThan = function ( number0, number1 ) {
 // this function can now be used over and over
 isGreaterThan( 120, 1);
 */
+
+/*
+ Arrays - video 7/9
+
+when refrencing the first index item it is called the 0th (zero-ith) or sub 0
+*/
+
+/*Prototypes*/
+
+// Constructor Function
+function Dog ( name, breed, tricksArray) {
+  this.name = name;
+  this.breed = breed;
+  this.tricks = tricksArray;
+  this.paws = 4;
+  this.domesticated = true;
+  this.bark = function( ) {
+    alert( "Hey! There might be something over there!");
+  }
+}
+
+var puppy = new Dog( 'Banjo', 'Pitbull', ['sits', 'lays down', 'stays']);
+
+console.log( puppy );
+
+console.log("My dog's name is " + puppy.name);
+puppy.bark();
+
+yourDog = new Dog("Lola", 'Cattle Dog', ['Dance', 'Roll Over']);
+
+console.log(yourDog);
