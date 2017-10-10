8fc79d49b678f40b1c821279aedec6f6de08f03c
Hunter Rancourt <skwid138@gmail.com>
2017-10-10 10:54:16 -0500

    updated notes

--- class_notes.js
+++ class_notes.js
@@ -37,7 +37,7 @@ Mocha
 npm install --global mocha (install globally)
 npm install --save dev mocha (makes it a dev dependency on a project)
 
-npm install mocha chai --save dev
+npm install mocha chai --save-dev
 
 NEW FILE STRUCTURE
 ** on root **
@@ -72,6 +72,36 @@ describe('test for [moduleName]', function() {
     "start": "node src/server.js",
     "test": "mocha test/"
 }
+
+Can also test server routes
+
+npm install request --save-dev
+
+**** server.js ****
+[listening with all requires including request and a /get]
+
+
+**** server.test.js ****
+var request = require('../src/modules/[moduleName]');
+var expect = require('chai').expect;
+
+var url = 'http:///localhost:3000/[moduleName]/[module argument]
+
+describe('test for server file server.js', function() {
+    describe('module route', function() {
+        it('should do what module should do', function(done) {
+            request.(url, function(err, response, body) {
+                console.log('body ->', body);
+                // have to parse the body from JSON to a JS Obj
+                expect(JSON.parse(body).result).to.eqaul([what module return should be]);
+                // both expects need to be true for the test to pass
+                expect(response.stsatusCode.to.be.equal(200));
+                done();
+            }) // end request
+        }) // end it
+    }) // end 2nd describe
+
+}); // end describe
 
  */
 
